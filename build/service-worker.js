26e9675ac1b2c875c20fc1e79bf2a4c230d2b4e2
Fsss126 <32033964+Fsss126@users.noreply.github.com>
2019-12-01 09:22:54 +0300

    Stable build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.77f79d005deca81c7ff01d427ff0675d.js"
+  "./precache-manifest.07c08ab8801a99071346e35fbf063658.js"
 );
 
 self.addEventListener('message', (event) => {
